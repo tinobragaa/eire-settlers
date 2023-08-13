a0896481e015f90966be68c71a55cf8b483bd06e
Valentino Braga <valentinobragaa@gmail.com>
2023-08-13 02:13:40 +0000

    Fix heroku URL

--- eire_settlers/settings.py
+++ eire_settlers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eire-settlers.herokuapp.com', 'localhost', '8000-tinobragaa-eiresettlers-7eh6bd9drj6.ws-eu103.gitpod.io']
+ALLOWED_HOSTS = ['eire-settlers-9b0e3e0c192c.herokuapp.com', 'localhost']
 
 
 # Application definition
