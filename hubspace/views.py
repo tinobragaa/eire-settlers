39ac96fd998d841ae03443536801412d0484c527
Valentino Braga <valentinobragaa@gmail.com>
2023-09-29 20:23:20 +0000

    Add: ArticlesList view created and connected urls file

--- hubspace/views.py
+++ hubspace/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Articles
 
-# Create your views here.
+
+class ArticlesList(generic.ListView):
+    model = Articles
+    queryset = Articles.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
