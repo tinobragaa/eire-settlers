87f95ff688abd9bfec23f5ae9649537516549431
Valentino Braga <valentinobragaa@gmail.com>
2023-09-29 13:27:13 +0000

    Add: wysiwyg editor and auto fill slug

--- eire_settlers/settings.py
+++ eire_settlers/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eire-settlers-9b0e3e0c192c.herokuapp.com', 'localhost', '8000-tinobragaa-eiresettlers-7eh6bd9drj6.ws-eu103.gitpod.io']
+ALLOWED_HOSTS = ['eire-settlers-9b0e3e0c192c.herokuapp.com', 'localhost', '8000-tinobragaa-eiresettlers-dmy2iu9lqfn.ws-eu105.gitpod.io']
 
-CSRF_TRUSTED_ORIGINS=['https://8000-tinobragaa-eiresettlers-7eh6bd9drj6.ws-eu103.gitpod.io', 'https://eire-settlers-9b0e3e0c192c.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS=['https://8000-tinobragaa-eiresettlers-dmy2iu9lqfn.ws-eu105.gitpod.io', 'https://eire-settlers-9b0e3e0c192c.herokuapp.com/']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'hubspace',
 ]
 
--- hubspace/admin.py
+++ hubspace/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Articles
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-admin.site.register(Articles)
+
+@admin.register(Articles)
+class ArticlesAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
