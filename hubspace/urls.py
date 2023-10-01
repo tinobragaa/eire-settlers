8cabc28f4a45706e9c8046112159b2fe718c29d6
Valentino Braga <valentinobragaa@gmail.com>
2023-10-01 07:58:09 +0000

    Add: ArticleDetail view created

--- hubspace/urls.py
+++ hubspace/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ArticlesList.as_view(), name='home'),
+    path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
 ]
--- hubspace/views.py
+++ hubspace/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Articles
 
 
@@ -8,3 +8,25 @@ class ArticlesList(generic.ListView):
     queryset = Articles.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+class ArticleDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Articles.objects.filter(status=1)
+        articles = get_object_or_404(queryset, slug=slug)
+        comments = (
+            articles.comments.filter(approved=True).order_by("-created_on"))
+        endorsed = False
+        if articles.endorsement.filter(id=self.request.user.id).exists():
+            endorsed = True
+
+        return render(
+            request,
+            "article_detail.html",
+            {
+                "articles": articles,
+                "comments": comments,
+                "endorsed": endorsed
+            },
+        )
