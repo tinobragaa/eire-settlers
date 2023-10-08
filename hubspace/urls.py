4728be73241d66b4b694dec8ba85a6ddc73ebb25
Valentino Braga <valentinobragaa@gmail.com>
2023-10-08 11:51:01 +0000

    Add: connected urls for MemberProfile view

--- hubspace/urls.py
+++ hubspace/urls.py
@@ -3,7 +3,16 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ArticlesList.as_view(), name='home'),
+    path('profile/', views.MemberProfile.as_view(), name="member_profile"),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
-    path('endorse/<slug:slug>', views.ArticlesEndorsement.as_view(), name="articles_endorsement"),
-    path('save/<slug:slug>', views.SavedArticle.as_view(), name="saved_article"),
+    path(
+        'endorse/<slug:slug>',
+        views.ArticlesEndorsement.as_view(),
+        name="articles_endorsement"
+        ),
+    path(
+        'save/<slug:slug>',
+        views.SavedArticle.as_view(),
+        name="saved_article"
+        ),
 ]
