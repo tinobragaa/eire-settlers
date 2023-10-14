c25187e4dfcd5d664d74a64c737af665694fcaf0
Valentino Braga <valentinobragaa@gmail.com>
2023-10-14 18:17:17 +0000

    Add: wired up url for reportcomment view

--- hubspace/urls.py
+++ hubspace/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('profile/', views.MemberProfile.as_view(), name="member_profile"),
     path('update_profile/', views.update_profile, name='update_profile'),
     path('delete_profile/', views.delete_profile, name="delete_profile"),
-    path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('delete-comment/<int:comment_id>/', views.delete_comment, name="delete_comment"),
     path('edit-comment/<int:comment_id>/', views.edit_comment, name="edit_comment"),
+    path('report_comment/<int:pk>/', views.ReportComment.as_view(), name="report_comment"),
+    path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('endorse/<slug:slug>', views.ArticlesEndorsement.as_view(), name="articles_endorsement"),
     path('save/<slug:slug>', views.SavedArticle.as_view(), name="saved_article"),
 ]
