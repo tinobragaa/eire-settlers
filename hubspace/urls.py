dab19860d065a24371ee8d50b24739dc5b46bd02
Valentino Braga <valentinobragaa@gmail.com>
2023-10-15 16:47:54 +0000

    Add: wired up url for CreateArticle view

--- hubspace/urls.py
+++ hubspace/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('delete-comment/<int:comment_id>/', views.delete_comment, name="delete_comment"),
     path('edit-comment/<int:comment_id>/', views.edit_comment, name="edit_comment"),
     path('report_comment/<int:pk>/', views.ReportComment.as_view(), name="report_comment"),
+    path('create_article/', views.CreateArticle.as_view(), name="create_article"),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('endorse/<slug:slug>', views.ArticlesEndorsement.as_view(), name="articles_endorsement"),
     path('save/<slug:slug>', views.SavedArticle.as_view(), name="saved_article"),
