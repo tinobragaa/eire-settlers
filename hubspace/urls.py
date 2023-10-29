201461b5ed93d00b544a9c2c5488f05606b9fdee
Valentino Braga <valentinobragaa@gmail.com>
2023-10-29 07:23:58 +0000

    Add: wired up url for MemberArticles view

--- hubspace/urls.py
+++ hubspace/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('article_edit/<slug:slug>/', views.EditArticle.as_view(), name='edit_article'),
     path('delete/<slug:slug>/', views.DeleteArticle.as_view(), name='delete_article'),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
+    path('member_articles/<str:user>/', views.MemberArticles.as_view(), name='member_articles'),
     path('endorse/<slug:slug>', views.ArticlesEndorsement.as_view(), name="articles_endorsement"),
     path('save/<slug:slug>', views.SavedArticle.as_view(), name="saved_article"),
 ]
